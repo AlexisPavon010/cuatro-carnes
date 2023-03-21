cdc11be937213169315072186030e70d436519c1
Alexis Pavon <themaster034@gmail.com>
2023-03-21 13:07:55 -0300

    add footer component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,55 @@
+import { FaFacebookF, FaInstagram, FaYoutube, FaTwitter } from 'react-icons/fa';
+
+import styles from './styles.module.scss'
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__container}>
+        <div className={styles.footer__menu}>
+          <div className={styles.footer__menu_text}>
+          </div>
+          <div className={styles.footer__spacer}></div>
+          <div className={styles.footer__menu_info_start}>
+            <h5 className={styles.footer__menu_info_text}>Links Rápidos</h5>
+            <p className={styles.footer__menu_info_subtitle}>Menú</p>
+            <p className={styles.footer__menu_info_subtitle}>Locales</p>
+            <p className={styles.footer__menu_info_subtitle}>Ofertas</p>
+            <p className={styles.footer__menu_info_subtitle}>Nosotros</p>
+            <p className={styles.footer__menu_info_subtitle}>Cupones</p>
+          </div>
+          <div className={styles.footer__menu_info_center}>
+            <h5 className={styles.footer__menu_info_text}>Información</h5>
+            <p className={styles.footer__menu_info_subtitle}>Sitemap</p>
+          </div>
+          <div className={styles.footer__menu_info_end}>
+            <h5 className={styles.footer__menu_info_text}>Compañía</h5>
+            <p className={styles.footer__menu_info_subtitle}>Contáctanos</p>
+            <p className={styles.footer__menu_info_subtitle}>Empleo</p>
+            <p className={styles.footer__menu_info_subtitle}>Delivery</p>
+            <p className={styles.footer__menu_info_subtitle}>Política de privacidad</p>
+            <p className={styles.footer__menu_info_subtitle}>Defensa de las y los consumidores</p>
+          </div>
+        </div>
+        <div className={styles.footer__bottom}>
+          <span className={styles.footer__copyrights}>2023 Cuatro Carnes &reg; Todos los derechos reservados.</span>
+          <div className={styles.footer__spacer}></div>
+          <div className={styles.footer__social}>
+            <div className={styles.footer__social_icon}>
+              <FaFacebookF size={24} />
+            </div>
+            <div className={styles.footer__social_icon}>
+              <FaInstagram size={24} />
+            </div>
+            <div className={styles.footer__social_icon}>
+              <FaYoutube size={24} />
+            </div>
+            <div className={styles.footer__social_icon}>
+              <FaTwitter size={24} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer >
+  )
+}
