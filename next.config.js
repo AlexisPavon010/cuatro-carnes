129b5d8800872d2099642c61d106243739508607
Alexis Pavon <themaster034@gmail.com>
2023-03-21 13:11:46 -0300

    add assest

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['s3-sa-east-1.amazonaws.com']
+  },
 }
 
 module.exports = nextConfig
