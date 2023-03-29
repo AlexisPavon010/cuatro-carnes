6e8aa9cc32f04e2bfe990811b4583f4a3a0e292f
Alexis Pavon <themaster034@gmail.com>
2023-03-29 16:41:38 -0300

    add more providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,28 @@
 import '../public/antd.min.css';
 import '../styles/globals.scss';
 
-import type { AppProps } from 'next/app';
 import { StyleProvider } from '@ant-design/cssinjs';
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { SWRConfig } from 'swr';
+
+import { store } from '@/store';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <StyleProvider hashPriority='high'>
-      <Component {...pageProps} />
-    </StyleProvider>
+    <SessionProvider session={pageProps.session}>
+      <SWRConfig
+        value={{
+          fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+        }}
+      >
+        <Provider store={store}>
+          <StyleProvider hashPriority='high'>
+            <Component {...pageProps} />
+          </StyleProvider>
+        </Provider>
+      </SWRConfig>
+    </SessionProvider>
   );
 }
