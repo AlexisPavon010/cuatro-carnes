55855452c2488cea0c80fbaf17f61484dea73d3d
Alexis Pavon <themaster034@gmail.com>
2023-03-29 16:40:43 -0300

    add category model

--- models/Category.ts
+++ models/Category.ts
@@ -2,11 +2,12 @@ import mongoose, { Schema } from 'mongoose'
 
 const CategorySchema = new Schema({
   name: { type: String },
-  image: { type: String },
-  password: { type: String },
+  description: { type: String },
 }, {
   timestamps: true
 })
+
+CategorySchema.index({ name: 'text', description: 'text' });
 
 const Category = mongoose.models.Category || mongoose.model('Category', CategorySchema)
 
