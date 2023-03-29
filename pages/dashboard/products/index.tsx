9b18be79e42b1ad6d2c254457bb31896f508610f
Alexis Pavon <themaster034@gmail.com>
2023-03-29 16:32:21 -0300

    remove get server side props

--- pages/dashboard/products/index.tsx
+++ pages/dashboard/products/index.tsx
@@ -1,35 +1,18 @@
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
 
 import { Layout } from '@/components/Dashboard/Layout/Layout'
 import { ProductTable } from '@/components/Dashboard/ProductTable';
-import { getProducts } from '@/database';
-import { getCategories } from '@/database/getCategories';
 
-const DashboardPage = ({ products, categories }: any) => {
+const DashboardPage = () => {
 
   return (
     <Layout>
       <Head>
         <title>Dashboard - Productos | Cuatro Carnes</title>
       </Head>
-      <ProductTable data={products} categories={categories} />
+      <ProductTable />
     </Layout>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async ({ req, query }) => {
-
-  const products = await getProducts()
-  const categories = await getCategories()
-
-
-  return {
-    props: {
-      products,
-      categories
-    }
-  }
 }
 
 export default DashboardPage
