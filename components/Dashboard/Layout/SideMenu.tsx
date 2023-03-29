c9f2c3d940e77457c28883fba4b74cdf62fcd5f1
Alexis Pavon <themaster034@gmail.com>
2023-03-29 16:35:34 -0300

    add menu items

--- components/Dashboard/Layout/SideMenu.tsx
+++ components/Dashboard/Layout/SideMenu.tsx
@@ -1,22 +1,33 @@
 import { Divider, Menu } from "antd"
 import Image from "next/image"
 import { useRouter } from "next/router"
-import { BsBoxSeam, BsFillBoxFill, BsTicketPerforated } from "react-icons/bs"
+import { BsBoxSeam, BsTicketPerforated } from "react-icons/bs"
 import { MdOutlineComputer } from "react-icons/md"
 import { FiUsers } from "react-icons/fi"
 import { BiCategory } from "react-icons/bi"
-
+import { TfiMenuAlt } from "react-icons/tfi"
+import { RiUserSettingsLine } from "react-icons/ri"
 
 import styles from './styles.module.scss'
 
-export const SideMenu = () => {
+interface SideMenuProps {
+  collapsed: boolean;
+}
+
+export const SideMenu = ({ collapsed }: SideMenuProps) => {
   const router = useRouter()
 
   return (
     <div className={styles.side}>
-      <div className={styles.side__image_wrapper}>
-        <Image src='/assets/logo-cuatro-carnes.svg' alt='logo cuatro carnes' width={180} height={70} />
-      </div>
+      {collapsed ? (
+        <div className={styles.side__image_wrapper_logo}>
+          <Image src='/assets/logo-side.svg' alt='logo cuatro carnes' width={80} height={50} />
+        </div>
+      ) : (
+        <div className={styles.side__image_wrapper}>
+          <Image src='/assets/logo-cuatro-carnes.svg' alt='logo cuatro carnes' width={180} height={70} />
+        </div>
+      )}
       <Divider />
       <Menu
         className={styles.side__menu}
@@ -39,19 +50,31 @@ export const SideMenu = () => {
             key: '/dashboard/categories',
             icon: <BiCategory size={20} />,
             label: 'Categorias',
-            onClick: () => router.push('/dashboard')
+            onClick: () => router.push('/dashboard/categories')
+          },
+          {
+            key: '/dashboard/opciones',
+            icon: <TfiMenuAlt size={20} />,
+            label: 'Opciones',
+            onClick: () => router.push('/dashboard/options')
           },
           {
             key: '/dashboard/cupons',
             icon: <BsTicketPerforated size={20} />,
             label: 'Cupones',
-            onClick: () => router.push('/dashboard')
+            onClick: () => router.push('/dashboard/coupons')
           },
           {
             key: '/dashboard/clients',
             icon: <FiUsers size={20} />,
             label: 'Clientes',
-            onClick: () => router.push('/dashboard')
+            onClick: () => router.push('/dashboard/customers')
+          },
+          {
+            key: '/dashboard/users',
+            icon: <RiUserSettingsLine size={20} />,
+            label: 'Usuarios',
+            onClick: () => router.push('/dashboard/users')
           },
         ]}
       />
