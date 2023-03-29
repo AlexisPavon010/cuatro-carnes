38e734ec088bb752b70c18d2b4f71c5b67156b56
Alexis Pavon <themaster034@gmail.com>
2023-03-29 16:43:21 -0300

    add swr fetcher

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -4,8 +4,11 @@ import Head from 'next/head';
 import { Layout } from '@/components/Dashboard/Layout/Layout'
 import { Card } from '@/components/Dashboard/Card/Card';
 import { OrderTable } from '@/components/Dashboard/OrderTable';
+import { useSwrFetcher } from '@/hooks/useSwrFetcher';
 
 const DashboardPage = () => {
+  const { data, isLoading } = useSwrFetcher('/api/order')
+
   return (
     <Layout>
       <Head>
@@ -13,19 +16,39 @@ const DashboardPage = () => {
       </Head>
       <Row gutter={[16, 16]}>
         <Col xs={24} sm={12} lg={6}>
-          <Card total={2} title='Pedidos del Dia' description='Total de pedidos recibidos en el día' />
+          <Card
+            total={0}
+            loading={isLoading}
+            title='Pedidos del Dia'
+            description='Total de pedidos recibidos en el día'
+          />
         </Col>
         <Col xs={24} sm={12} lg={6}>
-          <Card total={0} title='Pedidos Entregados' description='Pedidos entregados del día' />
+          <Card
+            total={0}
+            loading={isLoading}
+            title='Pedidos Entregados'
+            description='Pedidos entregados del día'
+          />
         </Col>
         <Col xs={24} sm={12} lg={6}>
-          <Card total={7} title='Pedidos Pendientes' description='Falta entregar' />
+          <Card
+            total={7}
+            loading={isLoading}
+            title='Pedidos Pendientes'
+            description='Falta entregar'
+          />
         </Col>
         <Col xs={24} sm={12} lg={6}>
-          <Card total={0} title='Pedidos Cancelados' description='Cancelados' />
+          <Card
+            total={0}
+            loading={isLoading}
+            title='Pedidos Cancelados'
+            description='Cancelados'
+          />
         </Col>
         <Col xs={24}>
-          <OrderTable />
+          <OrderTable isLoading={isLoading} data={data.data} />
         </Col>
       </Row>
     </Layout>
