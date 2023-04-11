dfaf754b15aa297b6cd08c289ac6db7cab84a7a6
Alexis Pavon <themaster034@gmail.com>
2023-04-11 12:36:27 -0300

    add link to shop

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -2,6 +2,7 @@ import { MdDeliveryDining } from 'react-icons/md';
 import { FaStoreAlt } from 'react-icons/fa';
 
 import styles from './styles.module.scss'
+import Link from 'next/link';
 
 export const Main = () => {
   return (
@@ -15,34 +16,38 @@ export const Main = () => {
               </h1>
             </div>
             <div className={styles.hero__card_item}>
-              <div className={styles.hero__card_item_content}>
-                <div className={styles.hero__card_item_image}>
-                  <FaStoreAlt color='white' size={24} />
+              <Link href='/products'>
+                <div className={styles.hero__card_item_content}>
+                  <div className={styles.hero__card_item_image}>
+                    <FaStoreAlt color='white' size={24} />
+                  </div>
+                  <div className={styles.hero__card_item_text_wrap}>
+                    <h2 className={styles.hero__card_item_text}>
+                      Para retirar
+                    </h2>
+                    <p className={styles.hero__card_item_subtitle}>
+                      El pedido se retira en nuestros locales
+                    </p>
+                  </div>
                 </div>
-                <div className={styles.hero__card_item_text_wrap}>
-                  <h2 className={styles.hero__card_item_text}>
-                    Para retirar
-                  </h2>
-                  <p className={styles.hero__card_item_subtitle}>
-                    El pedido se retira en nuestros locales
-                  </p>
-                </div>
-              </div>
+              </Link>
             </div>
             <div className={styles.hero__card_item}>
-              <div className={styles.hero__card_item_content}>
-                <div className={styles.hero__card_item_image}>
-                  <MdDeliveryDining color='white' size={24} />
+              <Link href='/products'>
+                <div className={styles.hero__card_item_content}>
+                  <div className={styles.hero__card_item_image}>
+                    <MdDeliveryDining color='white' size={24} />
+                  </div>
+                  <div className={styles.hero__card_item_text_wrap}>
+                    <h2 className={styles.hero__card_item_text}>
+                      Para delivery
+                    </h2>
+                    <p className={styles.hero__card_item_subtitle}>
+                      El pedido se envia a su domicilio
+                    </p>
+                  </div>
                 </div>
-                <div className={styles.hero__card_item_text_wrap}>
-                  <h2 className={styles.hero__card_item_text}>
-                    Para delivery
-                  </h2>
-                  <p className={styles.hero__card_item_subtitle}>
-                    El pedido se envia a su domicilio
-                  </p>
-                </div>
-              </div>
+              </Link>
             </div>
           </div>
         </div>
