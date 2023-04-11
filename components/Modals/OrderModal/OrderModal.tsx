9562787d8af60366b0cb024fa28da96cd0ddb7c3
Alexis Pavon <themaster034@gmail.com>
2023-04-11 12:36:00 -0300

    add data to modal

--- components/Modals/OrderModal/OrderModal.tsx
+++ components/Modals/OrderModal/OrderModal.tsx
@@ -1,56 +1,65 @@
-import { addToCart } from "@/store/cart/shoppingSlice"
 import { Button, Modal } from "antd"
+import CurrencyFormat from 'react-currency-format';
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai"
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import styles from './styles.module.scss'
+import { IProduct } from "@/interfaces/products"
+import { addToCart } from "@/store/cart/shoppingSlice"
+import { useState } from "react";
 
 interface OrderModalProps {
-  open: boolean;
-  close: (value: boolean) => void
+  open: { visible: boolean, product: undefined | IProduct };
+  close: (value: { visible: boolean, product: undefined | IProduct }) => void
 }
 
 export const OrderModal = ({ open, close }: OrderModalProps) => {
-
+  const [count, setCount] = useState(1)
   const dispatch = useDispatch()
+
+  const { image, title, price, description } = open.product || {}
 
   const addItemToCart = () => {
     dispatch(addToCart({
-      _id: 3,
-      price: 2000,
-      quantity: 1,
-      name: 'Combo Whopper 2x1',
-      description: 'asdasdasdasdasd'
+      quantity: count,
+      ...open.product
     }))
-    close(false)
+    close({ visible: false, product: undefined })
   }
 
   return (
     <Modal
       className={styles.modal}
       width={1000}
-      open={open}
-      onCancel={() => close(false)}
+      open={open.visible}
+      onCancel={() => close({ visible: false, product: undefined })}
       footer={<div></div>}
     >
       <div className={styles.modal__container}>
         <div className={styles.modal__image_content}>
-          <div className={styles.modal__image}></div>
+          <div
+            className={styles.modal__image}
+            style={{
+              backgroundImage: `url(${image})`
+            }}
+          ></div>
         </div>
         <div className={styles.modal__content}>
           <div className={styles.modal__card}>
-            <h2 className={styles.modal__title}>80% OFF en el segundo Combo Italiano Pategrás XL BK3</h2>
-            <div className={styles.modal__subtitle}>2,400.00</div>
-            <p>2 Combos con carne a la parrilla, pan, queso pategrás, tomate y mayonesa, acompañado con papas regulares y gaseosas regulares.</p>
+            <h2 className={styles.modal__title}>{title}</h2>
+            <div className={styles.modal__subtitle}>
+              <CurrencyFormat value={price} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+            </div>
+            <p>{description}</p>
             <hr className={styles.divider} />
             <div className={styles.modal__quantity}>
               <div className={styles.modal__quantity_actions}>
-                <h3>Cantidad: 1</h3>
+                <h3>Cantidad: {count}</h3>
                 <div className={styles.modal__quantity_buttons}>
-                  <Button size="large" shape="circle">
+                  <Button onClick={() => setCount((value) => value < 1 ? 0 : value - 1)} size="large" shape="circle">
                     <AiOutlineMinus size={24} color='grey' />
                   </Button>
-                  <Button size="large" shape="circle" >
+                  <Button onClick={() => setCount(count + 1)} size="large" shape="circle" >
                     <AiOutlinePlus size={24} color='grey' />
                   </Button>
                 </div>
@@ -60,7 +69,9 @@ export const OrderModal = ({ open, close }: OrderModalProps) => {
           <div className={styles.button__wrapper}>
             <button onClick={addItemToCart} className={styles.button}>
               <span className={styles.button__content}>
-                <div>$ 2,400.00</div>
+                <div>
+                  <CurrencyFormat value={price! * count} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                </div>
                 <div>Añadir</div>
               </span>
             </button>
