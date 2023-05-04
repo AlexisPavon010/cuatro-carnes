2bbfe8ae9f654af58afa16d088cb02992cafd03d
Alexis Pavon <themaster034@gmail.com>
2023-05-04 18:55:56 -0300

    finish add options

--- components/Dashboard/Drawers/AddOption.tsx
+++ components/Dashboard/Drawers/AddOption.tsx
@@ -1,10 +1,9 @@
 import { Button, Drawer, Form, Input, List, Modal, Space, Tooltip, Typography } from "antd"
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useState, useEffect } from "react";
+import { BsPencil, BsTrash } from "react-icons/bs";
 import { KeyedMutator } from "swr";
 
-import { createCategory } from "@/client";
-import { FiDelete } from "react-icons/fi";
-import { BsPencil, BsTrash } from "react-icons/bs";
+import { createItemOptions, createOptions, deletedItemOptions, getItemOptionById, getOptionById, updateItemOptions, updateOptions } from "@/client";
 
 interface AddOptionProps {
   mutate: KeyedMutator<any>
@@ -18,19 +17,27 @@ interface AddOptionProps {
   }
 }
 
-const data = [
-  'Opcion 1',
-  'Opcion 2',
-  'Opcion 3',
-  'Opcion 4',
-  'Opcion 5',
-];
-
 export const AddOption = ({ open, onClose, mutate }: AddOptionProps) => {
   const [form] = Form.useForm();
   const [openModal, setOpenModal] = useState(false);
+  const [openEditModal, setOpenEditModal] = useState({ visible: false, itemId: undefined });
+  const [openDeletedModal, setOpenDeletedModal] = useState({ visible: false, itemId: undefined });
   const [loading, setLoading] = useState(false);
   const [quantity, setQuantity] = useState(1)
+
+  const [options, setOptions] = useState<string[]>([]);
+
+  useEffect(() => {
+    form.resetFields()
+    if (!open.id) return
+    getOptionById(open.id)
+      .then(({ data }) => {
+        console.log(data)
+        setOptions(data.items)
+        form.setFieldsValue(data)
+      })
+      .catch((error) => console.log(error))
+  }, [open.id])
 
   return (
     <Drawer
@@ -54,7 +61,18 @@ export const AddOption = ({ open, onClose, mutate }: AddOptionProps) => {
         }}
         onFinish={(values) => {
           setLoading(true)
-          createCategory(values)
+          if (!open.id) {
+            createOptions({ ...values, quantity: quantity, items: options })
+              .then((response) => {
+                mutate(null)
+                form.resetFields()
+                onClose({ id: undefined, visible: false })
+              })
+              .catch((error) => console.log(error))
+              .finally(() => setLoading(false))
+          }
+
+          updateOptions(open.id!, values)
             .then((response) => {
               mutate(null)
               form.resetFields()
@@ -67,8 +85,8 @@ export const AddOption = ({ open, onClose, mutate }: AddOptionProps) => {
         requiredMark={false}
       >
         <Form.Item
-          label="Nombre de la Opción"
           name="name"
+          label="Nombre de la Opción"
           rules={[{ required: true, message: 'Porfavor inserte un nombre.' }]}
         >
           <Input placeholder='Escribe el nombre del producto' size='large' />
@@ -87,19 +105,29 @@ export const AddOption = ({ open, onClose, mutate }: AddOptionProps) => {
         <Space style={{ width: '100%' }} size='large' direction="vertical">
           <List
             bordered
-            dataSource={data}
-            renderItem={(item) => (
+            dataSource={options}
+            renderItem={(item: any) => (
               <List.Item
                 actions={[
                   <Tooltip title="Editar">
-                    <Button style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} type="text" shape="circle" icon={<BsPencil size={18} />} />
+                    <Button
+                      onClick={() => setOpenEditModal({ visible: true, itemId: item._id })}
+                      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                      type="text" shape="circle"
+                      icon={<BsPencil size={18} />}
+                    />
                   </Tooltip>,
                   <Tooltip title="Eliminar">
-                    <Button style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} type="default" shape="circle" icon={<BsTrash size={18} />} />
+                    <Button
+                      onClick={() => setOpenDeletedModal({ visible: true, itemId: item._id })}
+                      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                      type="default" shape="circle"
+                      icon={<BsTrash size={18} />}
+                    />
                   </Tooltip>,
                 ]}
               >
-                <Typography.Text mark>{item}</Typography.Text>
+                <Typography.Text mark>{item.name}</Typography.Text>
               </List.Item>
             )}
           />
@@ -107,12 +135,14 @@ export const AddOption = ({ open, onClose, mutate }: AddOptionProps) => {
           <Button onClick={() => setOpenModal(true)} block type="default">Agregar un Item</Button>
         </Space>
       </Form>
-      <NewItemModal open={openModal} setOpenModal={setOpenModal} />
+      <NewItemModal id={open.id} open={openModal} setOpenModal={setOpenModal} setOptions={setOptions} />
+      <DeletedItemModal id={open.id} open={openDeletedModal} setOpenDeletedModal={setOpenDeletedModal} setOptions={setOptions} />
+      <EditItemModal id={open.id} open={openEditModal} setOpenEditModal={setOpenEditModal} setOptions={setOptions} />
     </Drawer>
   )
 }
 
-const NewItemModal = ({ open, setOpenModal }: any) => {
+const NewItemModal = ({ id, open, setOpenModal, setOptions }: any) => {
   const [form] = Form.useForm()
   return (
     <Modal
@@ -127,6 +157,16 @@ const NewItemModal = ({ open, setOpenModal }: any) => {
         requiredMark={false}
         onFinish={(values: any) => {
           console.log(values)
+          if (!id) {
+            setOptions((prevState: any) => [...prevState, values])
+            setOpenModal(false)
+          }
+          createItemOptions(id, values)
+            .then(({ data }) => {
+              setOptions(data.items);
+              setOpenModal(false)
+            })
+            .catch((error) => console.log(error))
         }}
         onFinishFailed={() => { }}
       >
@@ -147,5 +187,82 @@ const NewItemModal = ({ open, setOpenModal }: any) => {
         </Form.Item>
       </Form>
     </Modal>
+  )
+}
+
+const EditItemModal = ({ id, open, setOpenEditModal, setOptions }: any) => {
+  const [form] = Form.useForm()
+
+  useEffect(() => {
+    getItemOptionById(id, open.itemId)
+      .then(({ data }) => {
+        console.log(data)
+        form.setFieldsValue(data)
+      })
+      .catch((error) => console.log(error))
+  }, [open.itemId])
+
+
+  return (
+    <Modal
+      title='Editar item'
+      open={open.visible}
+      onOk={() => form.submit()}
+      onCancel={() => setOpenEditModal({ visible: false, itemId: undefined })}
+    >
+      <Form
+        layout='vertical'
+        form={form}
+        requiredMark={false}
+        onFinish={(values: any) => {
+          updateItemOptions(id, open.itemId, values)
+            .then(({ data }) => {
+              console.log(data.items)
+              setOptions(data.items)
+              setOpenEditModal({ visible: false, itemId: undefined })
+            })
+            .catch((error) => console.log(error))
+        }}
+        onFinishFailed={() => { }}
+      >
+        <Form.Item
+          label="Nombre"
+          name="name"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Precio"
+          name="price"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input />
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
+
+const DeletedItemModal = ({ id, open, setOptions, setOpenDeletedModal }: any) => {
+  return (
+    <Modal
+      title='Eliminar Item'
+      open={open.visible}
+      onOk={() => {
+        deletedItemOptions(id, open.itemId)
+          .then(({ data }) => {
+            console.log(data)
+            setOptions(data.items)
+            setOpenDeletedModal({ visible: false, itemId: undefined })
+          })
+          .catch((error) => console.log(error))
+      }}
+      onCancel={() => setOpenDeletedModal({ visible: false, itemId: undefined })}
+    >
+      <p>Estas seguro que desea eliminar este item?.
+        Una vez eliminado no se podra recuperar.</p>
+    </Modal >
   )
 }
