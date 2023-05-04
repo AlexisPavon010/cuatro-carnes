ddfb159bb6bddac24f30c0e392efc68ef0e3a461
Alexis Pavon <themaster034@gmail.com>
2023-05-04 18:55:33 -0300

    custom layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,17 +3,26 @@ import { useState } from 'react'
 import { Drawer } from "../Drawer"
 import { Footer } from "../Footer"
 import { Header } from "../Header"
+import Head from 'next/head'
 
 interface LayoutProps {
   children: JSX.Element | JSX.Element[]
+  title: string
+  description?: string
 }
 
-export const Layout = ({ children }: LayoutProps) => {
+export const Layout = ({ children, title, description }: LayoutProps) => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
   const openDrawer = () => setIsDrawerOpen(true)
   const closeDrawer = () => setIsDrawerOpen(false)
   return (
     <>
+      <Head>
+        <title>{title}</title>
+        {description && (
+          <meta name="description" content={description} />
+        )}
+      </Head>
       <Header openDrawer={openDrawer} />
       {children}
       <Drawer
