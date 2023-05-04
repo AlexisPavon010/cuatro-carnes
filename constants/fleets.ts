4bbb4e5c25f62b3bf8c4c730bcb55a31094966c1
Alexis Pavon <themaster034@gmail.com>
2023-05-04 19:03:52 -0300

    add fleets

---
+++ constants/fleets.ts
@@ -0,0 +1,22 @@
+export const FLEETS = [
+  {
+    value: 'yellow',
+    label: 'Amarillo',
+    color: '#fadb14'
+  },
+  {
+    value: 'blue',
+    label: 'Azul',
+    color: '#1677ff'
+  },
+  {
+    value: 'green',
+    label: 'Verde',
+    color: '#52c41a'
+  },
+  {
+    value: 'default',
+    label: 'Blanco',
+    color: 'default'
+  },
+]
