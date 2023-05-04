476148fa4fdd72b832566a16aba2c8f7f8dc5239
Alexis Pavon <themaster034@gmail.com>
2023-05-04 19:06:26 -0300

    add recovery password page

--- components/Modals/RecoveryPassword/RecoveryPassword.tsx
+++ components/Modals/RecoveryPassword/RecoveryPassword.tsx
@@ -1,6 +1,9 @@
-import { Form, Input, Modal } from 'antd'
+import { Form, Input, Modal, Result } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons';
+import { useState } from 'react'
 
 import styles from './styles.module.scss'
+import { recoveryPassword } from '@/client';
 
 interface RecoveryPasswordProps {
   showRecovery: boolean;
@@ -8,32 +11,66 @@ interface RecoveryPasswordProps {
 }
 
 export const RecoveryPassword = ({ showRecovery, setShowRecovery }: RecoveryPasswordProps) => {
+  const [result, setResult] = useState(false)
+  const [loading, setLoading] = useState(false)
+
   return (
     <Modal
       open={showRecovery}
       onCancel={() => setShowRecovery(false)}
       footer={<></>}
     >
-      <div className={styles.recovery__container}>
-        <h2 className={styles.recovery__text}>VAMOS A AYUDARTE</h2>
-        <p className={styles.recovery__description}>Introduce tu dirección de correo electrónico y te mandaremos un correo electrónico con instrucciones para restablecer tu contraseña.</p>
-        <Form
-          onFinish={() => { }}
-          onFinishFailed={() => { }}
-        >
-          <Form.Item
-            name="username"
-            wrapperCol={{ offset: 2, span: 20 }}
-            rules={[{ required: true, message: 'Por favor inserte su correo.' }]}
+      {result ? (
+        <Result
+          status="success"
+          title="¡Solicitud realizada con éxito!"
+          subTitle="Por favor, revise su correo electrónico para continuar con el proceso de restablecimiento de contraseña."
+        />
+      ) : (
+        <div className={styles.recovery__container}>
+          <h2 className={styles.recovery__text}>VAMOS A AYUDARTE</h2>
+          <p className={styles.recovery__description}>Introduce tu dirección de correo electrónico y te mandaremos un correo electrónico con instrucciones para restablecer tu contraseña.</p>
+          <Form
+            onFinish={({ email }) => {
+              setLoading(true)
+              recoveryPassword(email)
+                .then(({ data }) => {
+                  setResult(true)
+                  setTimeout(() => {
+                    setShowRecovery(false)
+                    setResult(false)
+                  }, 4000);
+                })
+                .catch((error) => {
+                  setLoading(false)
+                  console.log(error)
+                })
+                .finally(() => {
+                  setLoading(false)
+                })
+            }}
           >
-            <Input type='email' placeholder='Correo' />
-          </Form.Item>
+            <Form.Item
+              name="email"
+              wrapperCol={{ offset: 2, span: 20 }}
+              rules={[{ required: true, message: 'Por favor inserte su correo.' }]}
+            >
+              <Input disabled={loading} type='email' placeholder='Correo' />
+            </Form.Item>
 
-          <Form.Item wrapperCol={{ offset: 6 }}>
-            <button type='submit' className={styles.recovery__button}> Continuar </button>
-          </Form.Item>
-        </Form>
-      </div>
-    </Modal>
+            <Form.Item wrapperCol={{ offset: 6 }}>
+              <button type='submit' className={styles.recovery__button}>
+                {
+                  loading
+                    ? <LoadingOutlined className={styles.recovery__button_loading} />
+                    : 'Continuar'
+                }
+              </button>
+            </Form.Item>
+          </Form>
+        </div>
+      )
+      }
+    </Modal >
   )
 }
