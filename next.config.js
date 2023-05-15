495d20a0d7e2b71ddd430469137b9beccbb131d7
Alexis Pavon <themaster034@gmail.com>
2023-05-15 08:03:58 -0300

    add html loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  future: {
+    webpack5: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.html$/,
+      loader: 'html-loader',
+    });
+    return config
+  },
   transpilePackages: ['antd'],
   images: {
     domains: ['s3-sa-east-1.amazonaws.com', 'lh3.googleusercontent.com', 'cdn.sanity.io', 'res.cloudinary.com']
