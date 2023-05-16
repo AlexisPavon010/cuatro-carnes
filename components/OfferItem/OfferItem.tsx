3cae59a3e9dad727d4275a00465e316c57a4797e
Alexis Pavon <themaster034@gmail.com>
2023-05-16 20:17:29 -0300

    add add to cart function

--- components/OfferItem/OfferItem.tsx
+++ components/OfferItem/OfferItem.tsx
@@ -1,15 +1,35 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux';
+import { BsCartPlus } from 'react-icons/bs';
 
 import styles from './styles.module.scss';
 import { IProduct } from '@/interfaces/products';
+import { addToCart } from '@/store/cart/shoppingSlice'
 
 interface OfferItemProps {
   product: IProduct
 }
 
 export const OfferItem = ({ product }: OfferItemProps) => {
+  const dispatch = useDispatch()
+  const router = useRouter()
+
+  const handleAddToCart = () => {
+    dispatch(addToCart({
+      quantity: 1,
+      ...product,
+      options: [],
+      price: product.offert_price,
+    }))
+    router.push('/checkout')
+  }
+
   return (
     <div className={styles.slider__list_item} >
+      <div className={styles.slider__list_item_plus} onClick={handleAddToCart} >
+        <BsCartPlus color='white' size={20} />
+      </div>
       <div className={styles.slider__list_item_image}>
         <Image src={product.image} width={150} height={120} alt='' />
         <div className={styles.slider__list_item_price}>
