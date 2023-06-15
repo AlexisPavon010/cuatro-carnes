054659ea19b403f3832f1e416ca1ccd49caad1dd
Alexis Pavon <themaster034@gmail.com>
2023-06-15 10:19:18 -0300

    remove menu info

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -11,19 +11,21 @@ export const Footer = () => {
           <div className={styles.footer__menu_text}>
           </div>
           <div className={styles.footer__spacer}></div>
-          <div className={styles.footer__menu_info_start}>
+          {/* <div className={styles.footer__menu_info_start}>
             <h5 className={styles.footer__menu_info_text}>Links Rápidos</h5>
             <p className={styles.footer__menu_info_subtitle}>Menú</p>
             <p className={styles.footer__menu_info_subtitle}>Locales</p>
             <p className={styles.footer__menu_info_subtitle}>Nosotros</p>
-          </div>
-          <div className={styles.footer__menu_info_center}>
+          </div> */}
+          {/* <div className={styles.footer__menu_info_center}>
             <h5 className={styles.footer__menu_info_text}>Información</h5>
             <p className={styles.footer__menu_info_subtitle}>Sitemap</p>
-          </div>
+          </div> */}
           <div className={styles.footer__menu_info_end}>
             <h5 className={styles.footer__menu_info_text}>Compañía</h5>
-            <p className={styles.footer__menu_info_subtitle}>Contáctanos</p>
+            <Link href='/nosotros#contact' className={styles.footer__menu_info_subtitle}>
+              Contáctanos
+            </Link>
             <Link href='/politica-de-privacidad' className={styles.footer__menu_info_subtitle}>
               Política de privacidad
             </Link>
@@ -33,18 +35,22 @@ export const Footer = () => {
           <span className={styles.footer__copyrights}>2023 Cuatro Carnes &reg; Todos los derechos reservados.</span>
           <div className={styles.footer__spacer}></div>
           <div className={styles.footer__social}>
-            <div className={styles.footer__social_icon}>
+            {/* <div className={styles.footer__social_icon}>
               <FaFacebookF size={24} color='#f4e8e4' />
-            </div>
+            </div> */}
             <div className={styles.footer__social_icon}>
-              <FaInstagram size={24} color='#f4e8e4' />
+              <Link href='https://www.instagram.com/cuatro_carnes' passHref legacyBehavior>
+                <a target="_blank" rel="noopener noreferrer">
+                  <FaInstagram size={24} color='#f4e8e4' />
+                </a>
+              </Link>
             </div>
-            <div className={styles.footer__social_icon}>
+            {/* <div className={styles.footer__social_icon}>
               <FaYoutube size={24} color='#f4e8e4' />
             </div>
             <div className={styles.footer__social_icon}>
               <FaTwitter size={24} color='#f4e8e4' />
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
