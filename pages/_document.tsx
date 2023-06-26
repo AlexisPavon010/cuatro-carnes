30393cf7147ddb41fa76fa5ecfd21d3c014b951f
Alexis Pavon <themaster034@gmail.com>
2023-06-25 22:34:36 -0300

    add google head script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link rel="icon" href="/assets/favicon.png" />
-        <link href='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css' rel='stylesheet' />
+        <Script
+          id="googlemaps"
+          type="text/javascript"
+          strategy="beforeInteractive"
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY}&libraries=places`}
+        />
       </Head>
       <body>
         <Main />
