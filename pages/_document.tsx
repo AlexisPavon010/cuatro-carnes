9c3eaac306cfbe2a84d91b76ee8906fdb9a4e4ba
Alexis Pavon <themaster034@gmail.com>
2023-07-27 21:03:40 -0300

    add gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,19 @@ export default function Document() {
           strategy="beforeInteractive"
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY}&libraries=places`}
         />
+        {/* Fragmento de código de Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
+              `,
+          }}
+        />
+        {/* Fin del fragmento de código de Google Analytics */}
       </Head>
       <body>
         <Main />
