8e4e09771831fa4aa461d48d66a7f3c9d38bcf5d
Alexispavon010 <themaster034@gmail.com>
2023-09-11 22:02:30 -0300

    add dropdown avatar

--- components/Dashboard/Layout/Navbar.tsx
+++ components/Dashboard/Layout/Navbar.tsx
@@ -1,36 +1,49 @@
-import { Breadcrumb, Button, Col, Row, Space } from "antd"
+import { Avatar, Breadcrumb, Button, Col, Dropdown, MenuProps, Row, Space } from "antd";
+import { signOut, useSession } from 'next-auth/react';
 import { AiOutlineMenu } from 'react-icons/ai';
-import { signOut, useSession } from 'next-auth/react'
 
-import styles from './styles.module.scss'
-import Link from "next/link";
+import styles from './styles.module.scss';
 
 interface NavbarProps {
   openMenu: () => void;
   items: any;
 }
 
-export const Navbar = ({ openMenu, items }: NavbarProps) => {
+export const Navbar = ({ openMenu, items: BreadItem }: NavbarProps) => {
   const { data: session } = useSession()
+
+  const items: MenuProps['items'] = [
+    {
+      label: 'Cerrar Sesion',
+      key: '3',
+      onClick: () => signOut()
+    },
+  ];
 
   return (
     <header className={styles.header}>
       <Row align="middle" gutter={16}>
-        <Col className={styles.header__menu} xs={1} md={0}>
+        <Col className={styles.header__menu} xs={2} md={0}>
           <Button type="link" onClick={openMenu} icon={<AiOutlineMenu size={24} color="#525F7F" />} />
         </Col>
-        <Col flex={1}>
+        <Col xs={0} sm={12} lg={24} flex={1}>
           <Breadcrumb
-            items={items}
+            items={BreadItem}
           />
         </Col>
+        <Col flex={1}>
+        </Col>
         <Col>
-          <Space size='large'>
-            <h3>{session?.user?.name}</h3>
-            <Button onClick={() => signOut()}>Cerrar Sesión</Button>
-          </Space>
+          <Dropdown placement="bottomRight" menu={{ items }} trigger={['click']}>
+            <Space>
+              {session?.user?.name}
+              <Avatar alt={session?.user?.name!} src={session?.user?.image}>
+                {session?.user?.name?.substring(0, 1)}
+              </Avatar>
+            </Space>
+          </Dropdown>
         </Col>
       </Row>
-    </header>
+    </header >
   )
 }
