1eaec61c4b4fa8cd5063f7204a637e95a1966560
Alexispavon010 <themaster034@gmail.com>
2023-09-17 21:23:26 -0300

    add function to add to cart

--- components/OfferItem/OfferItem.tsx
+++ components/OfferItem/OfferItem.tsx
@@ -3,25 +3,33 @@ import { BsCartPlus } from 'react-icons/bs';
 
 import styles from './styles.module.scss';
 import { IProduct } from '@/interfaces/products';
+import { addToCart } from '@/store/cart/shoppingSlice';
+import { useDispatch } from 'react-redux';
+import { setOpenCartDrawer } from '@/store/ui/uiSlice';
 
 interface OfferItemProps {
   product: IProduct;
-  setOpenModal?: (state: {
-    visible: boolean;
-    product: undefined | IProduct;
-  }) => void
 }
 
-export const OfferItem = ({ product, setOpenModal }: OfferItemProps) => {
+export const OfferItem = ({ product }: OfferItemProps) => {
   const { image, offert_price, title, stock } = product || {};
+  const dispatch = useDispatch()
+
+  const handleAddToCart = () => {
+    dispatch(addToCart({
+      quantity: 1,
+      ...product,
+      price: offert_price,
+      options: []
+    }))
+    dispatch(setOpenCartDrawer(true))
+  }
 
   return (
     <div className={styles.slider__list_item} >
-      {setOpenModal && (
-        <div className={styles.slider__list_item_plus} onClick={() => setOpenModal({ visible: true, product: { ...product, price: offert_price } })} >
-          <BsCartPlus color='white' size={20} />
-        </div>
-      )}
+      <div className={styles.slider__list_item_plus} onClick={handleAddToCart} >
+        <BsCartPlus color='white' size={20} />
+      </div>
       <div className={styles.slider__list_item_image}>
         <Image src={image} width={150} height={120} alt='' />
         <div className={styles.slider__list_item_price}>
