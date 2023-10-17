af2788d27cec2eba85e4838854b321068af5bb4e
Alexispavon010 <themaster034@gmail.com>
2023-10-17 20:12:13 -0300

    [fix]: show when there is no hat

--- pages/dashboard/order/[id].tsx
+++ pages/dashboard/order/[id].tsx
@@ -286,8 +286,8 @@ const OrderDetails = ({ order }: OrderDetailsProps) => {
           <Descriptions.Item label="Nombre">{order.username}</Descriptions.Item>
           <Descriptions.Item label="Email">{order.email}</Descriptions.Item>
           <Descriptions.Item label="Domicilio">{`${order.address} ` || '--------'}</Descriptions.Item>
-          <Descriptions.Item label="Piso/Depto/Lote">{`${order.reference} ` || '--------'}</Descriptions.Item>
-          <Descriptions.Item label="Localidad">{`${order.address_1} ` || '--------'}</Descriptions.Item>
+          <Descriptions.Item label="Piso/Depto/Lote">{`${order.reference ? order.reference : '--------'} `}</Descriptions.Item>
+          <Descriptions.Item label="Localidad">{`${order.address_1 ? order.address_1 : '--------'} `}</Descriptions.Item>
           <Descriptions.Item label="Celular">+{order.phone ? order.phone : '0000000'}</Descriptions.Item>
           <Descriptions.Item label="Para">
             <Tag color='blue'>{order.shipping === 'DELIVERY' ? 'DELIVERY' : 'RETIRAR'}</Tag>
